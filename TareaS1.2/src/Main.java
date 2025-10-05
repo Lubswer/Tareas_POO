bd1ee9c204630c3583e534c0752bfd2c06090ceb
lubswer <lubswer@hotmail.com>
2025-10-05 13:16:37 -0500

    Tarea2 Semana1

--- TareaS1.2/src/Main.java
+++ TareaS1.2/src/Main.java
@@ -2,7 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("//// TareaS1.2 semana 1 //// ");
+
+        System.out.println("//// TareaS1.2 semana  1 //// ");
         System.out.println("Nombre: Lubswer Alan Catagña Caiza");
         System.out.println("Paralelo: GR2");
 
