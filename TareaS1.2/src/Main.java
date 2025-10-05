f16f19869ac3e0b9fe452cfdd0e93c0d2fabdfc2
lubswer <lubswer@hotmail.com>
2025-10-05 11:53:57 -0500

    Tarea2 Semana1

---
+++ TareaS1.2/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("//// TareaS1.2 semana 1 //// ");
+        System.out.println("Nombre: Lubswer Alan Catagña Caiza");
+        System.out.println("Paralelo: GR2");
+
+    }
+}
